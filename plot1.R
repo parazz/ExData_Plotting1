a69fe2e88a9d3d83611362cea24a45e591780663
parazz <para-lol@hotmail.com>
2014-09-14 16:07:07 -0400

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,12 @@
+dataAll <- read.table("household_power_consumption.txt",
+                      header = TRUE,
+                      sep = ";",
+                      na.strings = "?",
+                      colClasses = c("character", "character", rep("numeric", 7)))
+data <- dataAll[dataAll$Date == "1/2/2007" | dataAll$Date == "2/2/2007", ]
+
+data$Date <- as.Date(data$Date,format='%d/%m/%Y')
+data$Time <- strptime(data$Time,format="%H:%M:%S")
+
+hist(data$Global_active_power,main="Global Active Power",col="red",xlab="Global Active Power(kilowatts)")
+png("plot1.png")
